b217ee95123a83e110dafff50c816e30d7d1ce69
Rain Catalpa <56062949+CatalpaRain@users.noreply.github.com>
2020-12-03 18:13:02 +0800

    Add files via upload

---
+++ TCPClient/TCPClient.cpp
@@ -0,0 +1,66 @@
+﻿#include<winsock2.h>
+#include<iostream>
+#include<string>
+#pragma warning(disable : 4996)
+
+using namespace std;
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA data;
+	if (WSAStartup(sockVersion, &data) != 0)
+	{
+		return 0;
+	}
+
+	while (true)
+	{
+		SOCKET sclient = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		if (sclient == INVALID_SOCKET)
+		{
+			cout << "invalid socket!" << endl;
+			return 0;
+		}
+
+		sockaddr_in serAddr;
+		serAddr.sin_family = AF_INET;
+		serAddr.sin_port = htons(8888);
+		serAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+		if (connect(sclient, (sockaddr*)&serAddr, sizeof(serAddr)) == SOCKET_ERROR)
+		{
+			//连接失败 
+			cout << "connect error !" << endl;
+			closesocket(sclient);
+			return 0;
+		}
+
+		string data;
+		cin >> data;
+		const char* sendData;
+		sendData = data.c_str();   //string转const char* 
+
+		/*
+		send()用来将数据由指定的socket传给对方主机
+		int send(int s, const void * msg, int len, unsigned int flags)
+			s为已建立好连接的socket，msg指向数据内容，len则为数据长度，参数flags一般设0
+			成功则返回实际传送出去的字符数，失败返回-1，错误原因存于error
+		*/
+		send(sclient, sendData, strlen(sendData), 0);
+
+		char recData[255];
+		int ret = recv(sclient, recData, 255, 0);
+		if (ret > 0)
+		{
+			recData[ret] = 0x00;
+			cout << recData << endl;
+		}
+		closesocket(sclient);
+	}
+
+	WSACleanup();
+
+	system("pause");
+	return 0;
+}
---
+++ TCPServer/TCPServer.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream> 
+#include <winsock2.h>  
+#pragma warning(disable : 4996)
+
+#pragma comment(lib,"ws2_32.lib")  
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	//初始化WSA  
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA wsaData;
+	if (WSAStartup(sockVersion, &wsaData) != 0)
+	{
+		return 0;
+	}
+
+	//创建套接字  
+	SOCKET slisten = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (slisten == INVALID_SOCKET)
+	{
+		cout << "create socket error !" << endl;
+		return 0;
+	}
+
+	//绑定IP和端口  
+	sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(8888);
+	sin.sin_addr.S_un.S_addr = INADDR_ANY;
+	if (bind(slisten, (LPSOCKADDR)&sin, sizeof(sin)) == SOCKET_ERROR)
+	{
+		cout << "bind error !" << endl;
+	}
+
+	//开始监听  
+	if (listen(slisten, 5) == SOCKET_ERROR)
+	{
+		cout << "listen error !" << endl;
+		return 0;
+	}
+
+	//循环接收数据  
+	SOCKET sClient;
+	sockaddr_in remoteAddr;
+	int nAddrlen = sizeof(remoteAddr);
+	char revData[255];
+	while (true)
+	{
+		cout << "阻塞。。。。等待连接。。。" << endl;
+		sClient = accept(slisten, (SOCKADDR*)&remoteAddr, &nAddrlen);
+		if (sClient == INVALID_SOCKET)
+		{
+			cout << "accept error !" << endl;
+			continue;
+		}
+
+		cout << "接受到一个连接：" << inet_ntoa(remoteAddr.sin_addr) << endl;
+
+		//接收数据  
+		int ret = recv(sClient, revData, 255, 0);
+		if (ret > 0)
+		{
+			revData[ret] = 0x00;
+			printf(revData);
+		}
+
+		//发送数据  
+		const char* sendData = "你好，TCP客户端！\n";
+		send(sClient, sendData, strlen(sendData), 0);
+		closesocket(sClient);
+	}
+
+	closesocket(slisten);
+	WSACleanup();
+	return 0;
+}
